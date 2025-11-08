dac4f44774ae781661c673a3cb6212c1c81c8979
Takuya-Mukai <takuya.mukai@gmail.com>
2025-11-08 20:02:54 +0900

    Update: molten movement

--- lua/Default/jupyter/init.lua
+++ lua/Default/jupyter/init.lua
@@ -228,8 +228,51 @@ require("lze").load({
 				end
 			end
 
-			-- { "<leader>md", "<cmd>MoltenDelete<cr>", { desc = "Delete molten cell" } },
-			-- { "<leader>mx", "<cmd>MoltenOpenInBrowser<cr>", { desc = "Open in browser" } },
+			-- 次のコードブロックの先頭にジャンプ (j)
+			local function go_to_next_code_block_start()
+				local start_pos = vim.api.nvim_win_get_cursor(0)
+				local current_line = start_pos[1]
+
+				-- 現在行の次から検索
+				vim.api.nvim_win_set_cursor(0, { current_line + 1, 0 })
+
+				-- ```<word> のパターン (インデント対応)
+				local pattern = "^\\s*```\\S\\+"
+
+				-- 'W' (ラップしない) フラグで順方向に検索
+				local found_pos = vim.fn.searchpos(pattern, "W")
+
+				if found_pos[1] > 0 then
+					-- 見つかった行の1行下 (コードの先頭) に移動
+					vim.api.nvim_win_set_cursor(0, { found_pos[1] + 1, 0 })
+					vim.cmd("normal! ^") -- 行頭の空白以外にジャンプ
+				else
+					-- 見つからなければ元の場所に戻る
+					vim.api.nvim_win_set_cursor(0, start_pos)
+				end
+			end
+
+			-- 前のコードブロックの先頭にジャンプ (k)
+			local function go_to_prev_code_block_start()
+				local start_pos = vim.api.nvim_win_get_cursor(0)
+				local current_line = start_pos[1]
+
+				-- 現在行の前から検索
+				vim.api.nvim_win_set_cursor(0, { current_line - 1, 0 })
+
+				local pattern = "^\\s*```\\S\\+"
+
+				-- 'bW' (後方 'b' + ラップしない 'W') フラグで逆方向に検索
+				local found_pos = vim.fn.searchpos(pattern, "bW")
+
+				if found_pos[1] > 0 then
+					-- 見つかった行の1行下 (コードの先頭) に移動
+					vim.api.nvim_win_set_cursor(0, { found_pos[1] + 1, 0 })
+					vim.cmd("normal! ^")
+				else
+					vim.api.nvim_win_set_cursor(0, start_pos)
+				end
+			end
 
 			local hydra = require("hydra")
 			hydra({
@@ -250,8 +293,8 @@ _j_/_k_: move down/up  _r_: run cell    _l_: run line  _R_: run above
 				mode = { "n" },
 				body = "<localleader>M", -- this is the key that triggers the hydra
 				heads = {
-					{ "j", ":MoltenNext<CR>" },
-					{ "k", ":MoltenPrev<CR>" },
+					{ "j", ":lua go_to_next_code_block_start()" },
+					{ "k", ":lua go_to_prev_code_block_start()" },
 					{ "r", ":QuartoSend<CR>" },
 					{ "l", ":QuartoSendLine<CR>" },
 					{ "R", ":QuartoSendAbove<CR>" },
