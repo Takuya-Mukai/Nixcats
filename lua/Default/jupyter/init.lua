421266c876410d97c79c66a285e688415d7d0ce0
m-tky <takuya.mukai@gmail.com>
2025-12-10 17:24:06 +0900

    feat: jovian-nvim update

--- lua/Default/jupyter/init.lua
+++ lua/Default/jupyter/init.lua
@@ -58,7 +58,7 @@ require("lze").load({
 				backend = "kitty",
 				processor = "magick_cli",
 				max_width_window_percentage = 100,
-				max_height_window_percentage = 50,
+				max_height_window_percentage = 100,
 				window_overlap_clear_enabled = true,
 			})
 		end,
